8187071ef3d62078502de1f1f67aa654258f5135
DarknezzONE <jankowski02@wp.pl>
2021-07-13 22:05:27 +0200

    ver 0.8

--- auto-lingo.py
+++ auto-lingo.py
@@ -1,4 +1,4 @@
-import sys, time, json
+import sys, time, json, argparse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -10,13 +10,21 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 
 def set_chrome_options(chrome_options):
-    # chrome_options.add_argument("--headless")
     chrome_options.add_argument("--log-level=3")
     chrome_options.add_argument("--disable-infobars")
-    # chrome_options.add_argument("start-maximized")
     chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--incognito")
-    chrome_options.add_experimental_option("prefs", {"profile.default_content_setting_values.notifications": 2})
+
+    if settings['incognito']:
+        chrome_options.add_argument("--incognito")
+
+    if settings['mute_audio']:
+        chrome_options.add_argument("--mute-audio")
+
+    if settings['maximize_window']:
+        chrome_options.add_argument("start-maximized")
+
+    if settings['headless']:
+        chrome_options.add_argument("--headless")
 
 
 def exit(message=""):
@@ -31,8 +39,8 @@ def exit(message=""):
 def get_settings():
     settings = {}
     try:
-	    with open('settings.json') as json_f:
-	        settings = json.load(json_f)
+        with open('settings.json') as json_f:
+            settings = json.load(json_f)
     except:
         print("Failed to import settings from settings.json.")
         sys.exit()
@@ -53,8 +61,30 @@ def get_credentials():
     return login, password
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--stories", help="stories mode", action="store_true")
+    parser.add_argument("-l", "--learn", help="learn mode", action="store_true")
+    parser.add_argument("-i", "--incognito", help="incognito browser mode", action="store_true")
+    parser.add_argument("-m", "--mute", help="mute browser audio", action="store_true")
+
+    args = parser.parse_args()
+
+    if args.incognito:
+        settings['incognito'] = True
+
+    if args.mute:
+        settings['mute_audio'] = True
+
+    # set default mode to stories
+    if not args.learn and not args.stories:
+        args.stories = True
+
+    return args
+
+
 def log_in(login, password):
-    if login != "" and password != "":
+    if settings['auto_login'] and login != "" and password != "":
         email_field = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, '//input[@data-test="email-input"]'))
         )
@@ -87,7 +117,7 @@ def task_tokens(tokens):
             tokens[i].click()
             tokens[j].click()
 
-            #check if we found a pair
+            # check if we found a pair
             classes = tokens[i].get_attribute('class')
             if '_3alTu' in classes:
                 done_list.append(i)
@@ -103,6 +133,90 @@ def task_options(options):
             pass
 
 
+def challenge_speak_listen():
+    skip = driver.find_element_by_xpath('//button[@data-test="player-skip"]')
+    skip.click()
+
+
+def challenge_form():
+    sentence = driver.find_element_by_xpath('//div[@data-test="challenge-form-prompt"]').get_attribute('data-prompt')
+    if sentence in dictionary:
+        choices = driver.find_elements_by_xpath('//div[@data-test="challenge-judge-text"]')
+
+        for choice in choices:
+            if choice.text == dictionary[sentence]:
+                choice.click()
+    else:
+        skip = driver.find_element_by_xpath('//button[@data-test="player-skip"]')
+        skip.click()
+        solution = driver.find_element_by_xpath('//div[@class="_1UqAr _1sqiF"]')
+        dictionary[sentence] = solution.text
+        print(sentence, '-x->', dictionary[sentence])
+
+
+def challenge_name():
+    sentence = driver.find_element_by_xpath('//h1[@data-test="challenge-header"]').text
+    print(sentence)
+    if sentence in dictionary:
+        input_field = driver.find_element_by_xpath('//input[@data-test="challenge-text-input"]')
+        input_field.send_keys(dictionary[sentence])
+        input_field.send_keys(Keys.RETURN)
+
+    else:
+        skip = driver.find_element_by_xpath('//button[@data-test="player-skip"]')
+        skip.click()
+        time.sleep(0.2)
+        solution = driver.find_element_by_xpath('//div[@class="_1UqAr _1sqiF"]').text
+        dictionary[sentence] = solution
+        print(sentence, '-+->', dictionary[sentence])
+
+
+def challenge_reverse_translation():
+    sentence = driver.find_element_by_xpath('//span[@data-test="hint-sentence"]').text
+    if sentence in dictionary:
+        input_field = driver.find_element_by_xpath('//input[@data-test="challenge-text-input"]')
+        input_field.send_keys(dictionary[sentence])
+        input_field.send_keys(Keys.RETURN)
+
+    else:
+        skip = driver.find_element_by_xpath('//button[@data-test="player-skip"]')
+        skip.click()
+        solution = driver.find_element_by_xpath('//span[@class="_3gI0Y"]').text
+        dictionary[sentence] = solution
+        print(sentence, '--->', dictionary[sentence])
+
+
+def challenge_translate():
+    sentence = driver.find_element_by_xpath('//span[@data-test="hint-sentence"]').text
+    if sentence in dictionary:
+        tap_tokens = driver.find_elements_by_xpath('//button[@data-test="challenge-tap-token"]')
+        if len(tap_tokens) > 0:
+            # get solution without dot at the end
+            solution = dictionary[sentence][:-1]
+            # remove commas and change string to lowercase
+            solution = solution.replace(",", " ").lower()
+            words = solution.split(" ")
+
+            for word in words:
+                for tap_token in tap_tokens:
+                    if "disabled" in tap_token.get_attribute('innerHTML'):
+                        continue
+                    if tap_token.text.lower() == word:
+                        tap_token.click()
+                        break
+        else:
+            input_field = driver.find_element_by_xpath('//textarea[@data-test="challenge-translate-input"]')
+            input_field.send_keys(dictionary[sentence])
+            input_field.send_keys(Keys.RETURN)
+
+    else:
+        skip = driver.find_element_by_xpath('//button[@data-test="player-skip"]')
+        skip.click()
+        solution = driver.find_element_by_xpath('//div[@class="_1UqAr _1sqiF"]').text
+        dictionary[sentence] = solution
+        print(sentence, '--->', dictionary[sentence])
+
+
 def complete_story():
     start_story = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, '//button[@data-test="story-start"]'))
@@ -113,19 +227,19 @@ def complete_story():
     story_completed = False
 
     while not story_completed:
-        #try to locate next button
+        # try to locate next button
         try:
             next = driver.find_element_by_xpath('//button[@data-test="stories-player-continue"]')
         except WebDriverException:
             break
 
-        #try to click next button
+        # try to click next button
         try:
             next.click()
         except WebDriverException:
             pass
         
-        #try to do any task
+        # try to do any task
         for task in task_list:
             options = driver.find_elements_by_xpath(task)
             if len(options) == 0:
@@ -140,14 +254,97 @@ def complete_story():
             next.click()
             break
     
-    #wait for completion to register
+    # wait for completion to register
     finish_story = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, '//button[@data-test="stories-player-done"]'))
     )
 
-    #close story tab and switch to main tab
+    # close story tab and switch to main tab
     driver.close()
     driver.switch_to.window(driver.window_handles[0])
+
+
+def complete_skill():
+    # wait for site to initialize
+    skip = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//button[@data-test="player-skip"]'))
+    )
+
+    skill_completed = False
+
+    while not skill_completed:
+        while True:
+            try:
+                no_thanks = driver.find_element_by_xpath('//button[@data-test="no-thanks-to-plus"]')
+                skill_completed = True
+                break
+            except WebDriverException:
+                pass
+
+            try:
+                challenge = driver.find_element_by_xpath('//div[@data-test="challenge challenge-speak"]')
+                challenge_speak_listen()
+                break
+            except WebDriverException:
+                pass
+
+            try:
+                challenge = driver.find_element_by_xpath('//div[@data-test="challenge challenge-listen"]')
+                challenge_speak_listen()
+                break
+            except WebDriverException:
+                pass
+
+            try:
+                challenge = driver.find_element_by_xpath('//div[@data-test="challenge challenge-listenTap"]')
+                challenge_speak_listen()
+                break
+            except WebDriverException:
+                pass
+
+            try:
+                challenge = driver.find_element_by_xpath('//div[@data-test="challenge challenge-form"]')
+                challenge_form()
+                break
+            except WebDriverException:
+                pass
+
+            try:
+                challenge = driver.find_element_by_xpath('//div[@data-test="challenge challenge-translate"]')
+                challenge_translate()
+                break
+            except WebDriverException:
+                pass
+                
+            try:
+                challenge = driver.find_element_by_xpath('//div[@data-test="challenge challenge-completeReverseTranslation"]')
+                challenge_reverse_translation()
+                break
+            except WebDriverException:
+                pass
+
+            try:
+                challenge = driver.find_element_by_xpath('//div[@data-test="challenge challenge-name"]')
+                challenge_name()
+                break
+            except WebDriverException:
+                pass
+
+            try:
+                next = driver.find_element_by_xpath('//button[@data-test="player-next"]')
+                next.click()
+                break
+            except WebDriverException:
+                pass
+
+        time.sleep(0.4)
+
+        # click continue
+        try:
+            next = driver.find_element_by_xpath('//button[@data-test="player-next"]')
+            next.click()
+        except WebDriverException:
+            pass
 
 
 def stories_bot():
@@ -175,10 +372,70 @@ def stories_bot():
 
             complete_story()
 
+            if settings['antifarm_sleep'] > 0:
+                time.sleep(settings['antifarm_sleep'])
+
+
+def learn_bot():
+    global dictionary
+    dictionary = {}
+
+    while True:
+        driver.get("https://www.duolingo.com/learn")
+        skills = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH, '//div[@data-test="skill"]'))
+        )
+
+        completed_skill = False
+
+        for skill in skills:
+            # search for g tag with grey circle fill
+            # cannot search for skills with level < 5 because some skills cap at level 1
+            g_tag = skill.find_element_by_tag_name('g')
+
+            if 'fill="#e5e5e5"' not in g_tag.get_attribute('innerHTML'):
+                continue
+
+            # before doing anything with skills, perform a blank click for possible notifications to disappear
+            blank_item = driver.find_element_by_xpath('//div[@class="_2fX2D"]')
+            blank_item.click()
+
+            # navigate to chosen skill
+            action = ActionChains(driver)
+            action.move_to_element(skill).perform()
+
+            time.sleep(0.5)
+
+            skill.click()
+
+            time.sleep(0.5)
+
+            start_skill = skill.find_element_by_xpath('//button[@data-test="start-button"]')
+
+            time.sleep(0.5)
+            action = ActionChains(driver)
+            action.move_to_element(start_skill).click().perform()
+
+            complete_skill()
+
+            completed_skill = True
+
+            if settings['antifarm_sleep'] > 0:
+                time.sleep(settings['antifarm_sleep'])
+
+            break
+
+        if not completed_skill:
+            break
+
 
 def main():
+    global settings
     settings = get_settings()
+
     login, password = get_credentials()
+
+    args = parse_arguments()
 
     chrome_options = Options()
     set_chrome_options(chrome_options)
@@ -197,8 +454,11 @@ def main():
 
     log_in(login, password)
 
-    stories_bot()
-    # learn_bot() TODO
+    if args.learn:
+        learn_bot()
+
+    if args.stories:
+        stories_bot()
 
     exit("Auto-lingo finished.")
 
